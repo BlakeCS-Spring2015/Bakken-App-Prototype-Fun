637f256b523ddfbf215f692ad33bed6ca1334554
Schmitty Smith <ajsmith15@blakeschool.org>
2015-02-09 13:17:59 -0600

    Initial Commit

---
+++ Bakken App Prototype Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bakken App Prototype Fun
+//
+//  Created by ajsmith15 on 2/9/15.
+//  Copyright (c) 2015 ajsmith15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bakken App Prototype FunTests/Bakken_App_Prototype_FunTests.m
@@ -0,0 +1,40 @@
+//
+//  Bakken_App_Prototype_FunTests.m
+//  Bakken App Prototype FunTests
+//
+//  Created by ajsmith15 on 2/9/15.
+//  Copyright (c) 2015 ajsmith15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Bakken_App_Prototype_FunTests : XCTestCase
+
+@end
+
+@implementation Bakken_App_Prototype_FunTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
